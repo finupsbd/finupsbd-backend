1777bbfaf505be8083d868c3e9f17cf0ea9cfc47
Shamimreza82 <shamimrezaone@gmail.com>
2025-05-09 17:34:53 +0600

    test application api genarate

--- dist/app/module/applicationForm/applicationForm.route.js
+++ dist/app/module/applicationForm/applicationForm.route.js
@@ -1,14 +1,25 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ApplicationRouter = void 0;
+const app_1 = require("./../../../app");
 const express_1 = __importDefault(require("express"));
 const applicationForm_controller_1 = require("./applicationForm.controller");
 const auth_1 = __importDefault(require("../../middleware/auth"));
 const validateRequest_1 = __importDefault(require("../../middleware/validateRequest"));
 const applicationForm_validation_1 = require("./applicationForm.validation");
+const catchAsync_1 = __importDefault(require("../../utils/catchAsync"));
 const route = express_1.default.Router();
 route.get('/', (0, auth_1.default)('USER', 'SUPER_ADMIN', 'ADMIN'), applicationForm_controller_1.ApplicationController.getSingleApplication);
 route.post('/', (0, auth_1.default)('USER', 'SUPER_ADMIN', 'ADMIN'), (0, validateRequest_1.default)(applicationForm_validation_1.ApplicationValidationSchema.CreateApplicationValidationSchema), applicationForm_controller_1.ApplicationController.createApplicationForm);
@@ -16,4 +27,87 @@ route.patch('/update-status/:id', (0, auth_1.default)('SUPER_ADMIN', 'ADMIN'), (
 route.get('/', applicationForm_controller_1.ApplicationController.getAllApplicationForm);
 route.post('/application-tracking', applicationForm_controller_1.ApplicationController.applicationTracking);
 // route.post('/application-forget', ApplicationController.applicationForget)
+/////test
+route.post('/step1', (0, auth_1.default)("USER", "SUPER_ADMIN"), (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { userId } = req.user;
+    console.log(userId);
+    const result = yield app_1.prisma.testApplication.create({
+        data: {
+            user: { connect: { id: userId } }, // you must have userId
+            TestBasicInfo: {
+                create: Object.assign({}, req.body),
+            },
+        },
+    });
+    console.log({ result });
+    res.json({
+        status: 'success',
+        message: 'Application created successfully',
+        data: result,
+    });
+})));
+route.post('/step2/:id', (0, auth_1.default)("USER", "SUPER_ADMIN"), (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { userId } = req.user;
+    const { id } = req.params;
+    console.log(userId);
+    const result = yield app_1.prisma.testContactInfo.create({
+        data: Object.assign(Object.assign({}, req.body), { testApplication: { connect: { id } } })
+    });
+    console.log({ result });
+    res.json({
+        status: 'success',
+        message: 'Application created successfully',
+        data: result,
+    });
+})));
+route.post('/step3/:id', (0, auth_1.default)("USER", "SUPER_ADMIN"), (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { userId } = req.user;
+    const { id } = req.params;
+    console.log(userId);
+    const result = yield app_1.prisma.testProfession.create({
+        data: Object.assign(Object.assign({}, req.body), { testApplication: { connect: { id } } })
+    });
+    console.log({ result });
+    res.json({
+        status: 'success',
+        message: 'Application created successfully',
+        data: result,
+    });
+})));
+route.get('/test/:id', (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    const result = yield app_1.prisma.testApplication.findUnique({
+        where: {
+            id: id,
+        },
+        include: {
+            TestBasicInfo: {
+                select: {
+                    name: true,
+                    age: true,
+                    dob: true,
+                }
+            },
+            TestContactInfo: {
+                select: {
+                    email: true,
+                    phone: true,
+                }
+            },
+            testProfession: {
+                select: {
+                    profession: true,
+                    monthlyIncome: true,
+                }
+            },
+            user: true,
+        }
+    });
+    console.log({ result });
+    res.json({
+        status: 'success',
+        message: 'Application created successfully',
+        data: result,
+    });
+})));
 exports.ApplicationRouter = route;
--- src/app/module/applicationForm/applicationForm.route.ts
+++ src/app/module/applicationForm/applicationForm.route.ts
@@ -1,9 +1,12 @@
+import { prisma } from './../../../app';
 
 import express from 'express';
 import { ApplicationController } from './applicationForm.controller';
 import auth from '../../middleware/auth';
 import validateRequest from '../../middleware/validateRequest';
 import { ApplicationValidationSchema } from './applicationForm.validation';
+import catchAsync from '../../utils/catchAsync';
+import { TMiddlewareUser } from '../../types/commonTypes';
 
 
 
@@ -35,4 +38,131 @@ route.patch(
 route.get('/', ApplicationController.getAllApplicationForm);
 route.post('/application-tracking', ApplicationController.applicationTracking)
 // route.post('/application-forget', ApplicationController.applicationForget)
+
+
+
+
+/////test
+
+route.post('/step1', auth("USER", "SUPER_ADMIN"), catchAsync(async (req, res) => {
+
+  const {userId} = req.user as TMiddlewareUser;
+
+  console.log(userId)
+
+   const result  = await prisma.testApplication.create({
+    data: {
+      user: { connect: { id: userId} }, // you must have userId
+      TestBasicInfo: {
+        create: {
+         ...req.body,
+        },
+      },
+    },
+  }); 
+
+  console.log({result})
+res.json({
+  status: 'success',
+  message: 'Application created successfully',
+  data: result,
+});
+
+  
+}))
+
+
+route.post('/step2/:id', auth("USER", "SUPER_ADMIN"), catchAsync(async (req, res) => {
+  const {userId} = req.user as TMiddlewareUser;
+  const { id } = req.params;
+
+  console.log(userId)
+
+   const result  = await prisma.testContactInfo.create({
+    data: {
+      ...req.body,
+      testApplication: { connect: { id} }, 
+    }
+   })
+
+  console.log({result})
+res.json({
+  status: 'success',
+  message: 'Application created successfully',
+  data: result,
+});
+}))
+
+
+route.post('/step3/:id', auth("USER", "SUPER_ADMIN"), catchAsync(async (req, res) => {
+  const {userId} = req.user as TMiddlewareUser;
+  const { id } = req.params;
+
+  console.log(userId)
+
+   const result  = await prisma.testProfession.create({
+    data: {
+      ...req.body,
+      testApplication: { connect: { id} }, 
+    }
+   })
+
+  console.log({result})
+res.json({
+  status: 'success',
+  message: 'Application created successfully',
+  data: result,
+});
+}))
+
+
+
+
+route.get('/test/:id', catchAsync(async (req, res) => {
+  const { id } = req.params;
+
+   const result  = await prisma.testApplication.findUnique({
+    where: {
+      id: id,
+    },
+    include: {
+      TestBasicInfo: {
+        select: {
+          name: true,
+          age: true,
+          dob: true,
+        }
+      },
+      TestContactInfo: {
+        select: {
+          email: true,
+          phone: true,
+        }
+      },
+      testProfession: {
+        select: {
+          profession: true,
+          monthlyIncome: true,
+        }
+      },
+      user: true,
+    }
+   })
+
+  console.log({result})
+res.json({
+  status: 'success',
+  message: 'Application created successfully',
+  data: result,
+});
+}))
+
+
+
+
+
+
+
+
+
 export const ApplicationRouter = route;
