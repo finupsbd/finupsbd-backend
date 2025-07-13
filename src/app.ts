44c4798f634b1e86d4d99b126991198cf12694f2
Shamimreza82 <shamimrezaone@gmail.com>
2025-07-14 00:34:57 +0600

    update root api

--- src/app.ts
+++ src/app.ts
@@ -55,7 +55,7 @@ app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
     status: true,
     message: 'FinupsBD server is up and running smoothly.',
-    developer: "Reza Shamim",
+    developer: "Reza",
     timestamp: new Date().toISOString(),
     uptime: process.uptime(), // in seconds
     server: {
